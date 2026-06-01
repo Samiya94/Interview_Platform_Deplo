24de2df4dca02b89cae5a3422b1bc5b4aef8a047
Samiya <shaikhsamiya1808@gmail.com>
2026-06-01 12:54:30 +0530

    Add debug logging for DB URL

--- src/main/java/com/interviewPlatform/InterviewPlatformApplication.java
+++ src/main/java/com/interviewPlatform/InterviewPlatformApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewPlatformApplication {
 
 	public static void main(String[] args) {
+		System.out.println("========== DEBUG STARTUP ==========");
+		System.out.println("ENV SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("ENV PORT: " + System.getenv("PORT"));
+		System.out.println("===================================");
 		SpringApplication.run(InterviewPlatformApplication.class, args);
 		System.out.println("Application Started successfully...");
 	}
